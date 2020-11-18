ce9a7292aefa5f873b6befd22228a595fa25087b
heuzin <matheusqs.arq@gmail.com>
2020-11-17 20:53:15 -0500

    filter for total expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpeseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters'
+import ExpenseSummary from './ExpensesSummary';
 
 const ExpenaseDashboardPage = () => (
     <div>
         <ExpenseListFilters />
+        <ExpenseSummary />
         <ExpeseList />
     </div>
 );
